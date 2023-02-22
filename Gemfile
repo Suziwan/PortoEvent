c9557db6178fcc171438e5b01a137566fcbb2c59
Suziwan <christe.suzanne@gmail.com>
2023-02-22 10:05:32 +0000

    Add gem mailcatcher

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.0.0"
 gem "faker"
 gem "dotenv-rails"
 gem "devise", github: "heartcombo/devise", branch: "main"
+gem "mailcatcher"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,11 @@ Rails.application.configure do
   # is an example of default_url_options appropriate for a development environment
   # in config/environments/development.rb:
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Gem mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
   
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
